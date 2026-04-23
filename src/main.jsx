4a1e194c9528df38cb6049b00b04ed09ef6df6d2
2710brian <info@marketinggroupmalaga.com>
2026-04-23 14:04:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-const App = () => {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>CRM Dashboard</h1>
-      <p>Det virker 🔥</p>
-    </div>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
